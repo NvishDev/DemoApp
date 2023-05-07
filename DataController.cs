d6b2e7a49ded86cd654895199eae832d0cd19b8e
NK <DeCrazyCoder@gmail.com>
2023-05-08 03:43:02 +0530

    test3

--- DataController.cs
+++ DataController.cs
@@ -2,17 +2,9 @@
 using Microsoft.AspNetCore.Mvc;
 
 namespace DemoApp
-{
-    [Route("api/[controller]")]
-    [ApiController]
+{  
     public class DataController : ControllerBase
     {
-        [HttpGet]
-        [Route("CheckStatus")]        
-        public string CheckAPIHealth()
-        {
-            return "API Working fine";
-
-        }
+       
     }
 }
