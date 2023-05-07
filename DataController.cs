521c821f9a717b2b37747651d6f7ce652b8acd8b
NK <DeCrazyCoder@gmail.com>
2023-05-08 01:04:00 +0530

    Testing commit

---
+++ DataController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoApp
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+        [HttpGet]
+        [Route("CheckStatus")]        
+        public string CheckAPIHealth()
+        {
+            return "API Working fine";
+        }
+    }
+}
