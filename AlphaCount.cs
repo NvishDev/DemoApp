272af9e8023e2cddac21650d739094498d787c0d
Diganta Barai <sesa603892@se.com>
2023-05-11 12:03:52 +0530

    Diganta Barai/MoonbellRomi - Test Project

---
+++ AlphaCount.cs
@@ -0,0 +1,8 @@
+﻿namespace DemoApp
+{
+    public class AlphaCount
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
---
+++ DataController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoApp
+{  
+    [ApiController]
+    [Route("[controller]")]
+    public class DataController : ControllerBase
+    {
+        //test
+        public DataController()
+        {
+
+        }
+
+        [HttpPost]
+        [Route("/api/Data/AlphaCoun")]
+        public Task<IActionResult> AlphaCountController([FromBody] string data) {
+
+            Dictionary<char, int> AlphaCounter = new Dictionary<char, int>();
+            foreach (var item in data.ToCharArray())
+            {
+                if(!AlphaCounter.ContainsKey(item)) AlphaCounter.Add(item, 1);
+                else AlphaCounter[item]++;
+            }
+
+            var alphaCounts = new List<AlphaCount>() {};
+            foreach (var item in AlphaCounter) {
+                alphaCounts.Add(new AlphaCount() { 
+                    Key = item.Key.ToString(),
+                    Value = item.Value.ToString(),
+                });
+            }
+ 
+            return Ok();
+        }
+
+    }
+}
