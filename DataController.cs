1f8a6c35b18ff15e5e38402387040563152ca7ba
NvishDev <132838809+NvishDev@users.noreply.github.com>
2023-05-12 10:25:43 +0530

    Update DataController.cs

--- DataController.cs
+++ DataController.cs
@@ -7,33 +7,7 @@ namespace DemoApp
     [Route("[controller]")]
     public class DataController : ControllerBase
     {
-        //test
-        public DataController()
-        {
-
-        }
-
-        [HttpPost]
-        [Route("/api/Data/AlphaCoun")]
-        public Task<IActionResult> AlphaCountController([FromBody] string data) {
-
-            Dictionary<char, int> AlphaCounter = new Dictionary<char, int>();
-            foreach (var item in data.ToCharArray())
-            {
-                if(!AlphaCounter.ContainsKey(item)) AlphaCounter.Add(item, 1);
-                else AlphaCounter[item]++;
-            }
-
-            var alphaCounts = new List<AlphaCount>() {};
-            foreach (var item in AlphaCounter) {
-                alphaCounts.Add(new AlphaCount() { 
-                    Key = item.Key.ToString(),
-                    Value = item.Value.ToString(),
-                });
-            }
- 
-            return Ok();
-        }
+        //test             
 
     }
 }
